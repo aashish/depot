a289cce9161d1b020d14a1cb4b0f4fe68d5ac319
aashish <aashishkiran8@yahoo.com>
2010-01-03 21:59:01 +0530

    -----------added--------------

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_depot_session',
+  :secret      => '5fa35496922394defe0eddaa24f13c771c96cf8ac46701e892768b5c71398722ea4b0f9617ea2e367158563b27e69eadaca1070d4274fb299a65d4a871aea010'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+ActionController::Base.session_store = :active_record_store
---
+++ db/migrate/20090514115819_create_line_items.rb
@@ -0,0 +1,20 @@
+class CreateLineItems < ActiveRecord::Migration
+  def self.up
+    create_table :line_items do |t|
+     #t.integer:product_id,:null=>false,:options=>
+#"CONSTRAINTfk_line_item_productsREFERENCESproducts(id)"
+#t.integer:order_id,:null=>false,:options=>
+#"CONSTRAINTfk_line_item_ordersREFERENCESorders(id)"
+      t.integer :product_id, :null => false
+      t.integer :order_id, :null => false
+      t.integer :quantity, :null => false
+      t.decimal :total_price, :null => false, :precision => 8, :scale => 2
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :line_items
+  end
+end
